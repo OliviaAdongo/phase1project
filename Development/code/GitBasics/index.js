66fd9dd068ed150b67cef9cff91df6c96b18fcd1
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-14 10:21:45 +0300

    Added

---
+++ Development/code/GitBasics/index.js
@@ -0,0 +1,51 @@
+function scuberGreetingForFeet(ride){
+    // Write your code here!
+    //var ride = 200 ;
+    
+    if(ride <= 400){
+      //let output = 'This one is on me!';
+  
+      return `This one is on me!`;
+    }
+  
+    else if (ride > 2000 && ride <= 2500){
+      
+       return `I will gladly take your thirty bucks.`;
+  
+    }
+    else if(ride > 2500){
+  
+      return `No can do.`;
+    }
+   
+  }
+  
+  function ternaryCheckCity(city){
+    if(city === 'NYC'){
+      return "Ok, sounds good.";
+  
+    }
+    else if (city !== 'NYC'){
+      return "No go.";
+    }
+    
+  }
+  
+  function switchOnCharmFromTip(tip){
+     if (tip === "generous"){
+     return "Thank you so much."
+     }
+    
+     else if(tip === "not as generous"){
+      return "Thank you."
+  
+    }
+    
+     else{
+       return "Bye.";
+     
+     }
+    
+  
+  
+  }  
